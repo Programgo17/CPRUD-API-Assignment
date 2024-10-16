a0b267b719149aab40e3e3a07902f06a44f76796
Programgo17 <157931925+Programgo17@users.noreply.github.com>
2024-10-16 12:12:48 -0400

    Update AnimalRepository.java

--- demo (3)/demo/src/main/java/com/csc340/demo/animal/AnimalRepository.java
+++ demo (3)/demo/src/main/java/com/csc340/demo/animal/AnimalRepository.java
@@ -2,6 +2,7 @@ package com.csc340.demo.animal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +12,7 @@ public interface AnimalRepository extends JpaRepository<Entity, Integer> {
     List<Entity> findBySpecies(String species);
 
     List<Entity> findByNameContaining(String namePart);
+
+    @Query(value = "SELECT * FROM animals_db", nativeQuery = true)
+    List<Entity> findAllAnimals();
 }
